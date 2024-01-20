7ebff42e7db161b2e9ea9a85afe1152796e976e3
AdrsP <144963769+AdrsP@users.noreply.github.com>
2024-01-20 03:17:42 +0000

    metodos funcionales pero no libres de falla

--- src/app.py
+++ src/app.py
@@ -6,6 +6,7 @@ from flask import Flask, request, jsonify, url_for
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from datastructures import FamilyStructure
+import json
 #from models import Person
 
 app = Flask(__name__)
@@ -31,12 +32,42 @@ def handle_hello():
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
     response_body = {
-        "hello": "world",
+        #"hello": "world",
         "family": members
     }
-
-
     return jsonify(response_body), 200
+
+# aca voy a agregar una ruta para traer un solo miembro
+
+@app.route('/member/<int:member_id>', methods=["GET"])
+def get_single_member(member_id):
+    member = jackson_family.get_member(member_id)
+
+    if (member != None and member != {}):
+        return jsonify(member), 200
+    else:
+        return jsonify({'message': 'No se encontro ese usuario'}), 400
+
+# ahora necesito agregar gente  
+
+@app.route('/member', methods=['POST'])
+def create_member():
+    body = json.loads(request.data)
+
+    if (body['age'] > 0 and type(body['lucky_numbers']) == list and type(body['first_name']) == str):
+        body['id'] = jackson_family._generateId()
+        jackson_family.add_member(body)
+        return jsonify({'message': 'miembro agregado'}), 200
+    else:
+        return jsonify({'message': 'alguno de los campos esta mal'}), 400
+
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def delete_members(member_id):
+    was_delete = jackson_family.delete_member(member_id)
+    if was_delete == True:
+        return jsonify({'message': 'ya no eres parte de esta familia'}), 200
+    else:
+        return jsonify({'message': 'no se pudo eliminar esa persona'}), 400
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/datastructures.py
+++ src/datastructures.py
@@ -13,7 +13,29 @@ class FamilyStructure:
         self.last_name = last_name
 
         # example list of members
-        self._members = []
+        self._members = [
+            {
+            "age": 33,
+            "first_name": "John",
+            "id": 1,
+            "lastname": last_name,
+            "lucky_numbers": [7, 13, 22]
+            },
+            {
+            "age": 35,
+            "first_name": "Jane",
+            "id": 2,
+            "lastname": last_name,
+            "lucky_numbers": [10, 14, 3]
+            },
+            {
+            "age": 5,
+            "first_name": "Jimmy",
+            "id": 3,
+            "lastname": last_name,
+            "lucky_numbers": [1]
+            }
+        ]
 
     # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
@@ -21,15 +43,24 @@ class FamilyStructure:
 
     def add_member(self, member):
         # fill this method and update the return
-        pass
+        # me da la impresion de lo que puedo hacer aca es recibir el objeto con la info del miembro y pues apend a la lista no?
+        member['lastname'] = self.last_name
+        self._members.append(member)
+        
 
     def delete_member(self, id):
         # fill this method and update the return
-        pass
+        # supongo que va a ser algo parecido al add member pero bueno ya va
+        for member in self._members:
+            if member['id'] == id:
+                self._members.remove(member) 
+                return True
 
     def get_member(self, id):
         # fill this method and update the return
-        pass
+        for member in self._members:
+            if member['id'] == id:
+                return member
 
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
